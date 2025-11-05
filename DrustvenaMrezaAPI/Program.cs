8ca924cac4c221a766cbc4deff13b870767fa093
Filip Knezevic <filipknezevic369@gmail.com>
2025-11-05 02:17:43 +0100

    config

--- DrustvenaMrezaAPI/Program.cs
+++ DrustvenaMrezaAPI/Program.cs
@@ -8,13 +8,24 @@ namespace DrustvenaMrezaAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    }
+                );
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            app.UseCors("AllowAllOrigins");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
